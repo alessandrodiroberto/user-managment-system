66dd8ee358e343cb8d440e98f30603eb3415316c
alessandrodiroberto <alessandro.diroberto@intecs.it>
2024-02-07 09:52:51 +0100

    use bootstrap from http

--- module-component/ums/src/app/app.module.ts
+++ module-component/ums/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersModule} from './users/users.module';
+import { ThemeTogglerComponent } from './theme-toggler/theme-toggler.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ThemeTogglerComponent,
   ],
   imports: [
     BrowserModule,
